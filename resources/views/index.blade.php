73e58d1aaf1587583b9919c82bb5b4b2149a96bc
patryk-uszynski <uszynski.patryk@gmail.com>
2017-07-07 02:28:24 +0200

    Master page layout separated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<style>
-		[v-cloak] {
-			display:none;
-		}
-		.jumbotron {
-			margin-top: 2rem;
-		}
-	</style>
-</head>
-<body class="container" > 
+@extends('master')
+
+@section('content')
 	<div class="col-md-8 offset-md-2" id="guestbook">
 		<div class="jumbotron">
 			<h1>Paste</h1>
@@ -38,12 +26,9 @@
 			<p><span class="btn btn-primary" v-on:click="onDelete(paste)">Delete</span></p>
 		</div>
 	</div>
+@endsection
 
-	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/vue"></script>
+@section('scripts')	
 	<script>
 		new Vue({ 
 			el: '#guestbook',
@@ -96,5 +81,4 @@
 			}
 		})
 	</script>
-</body>
-</html>
+@endsection
